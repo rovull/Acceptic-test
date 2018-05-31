5cc85f351aa6bb7e67256aa3cf017afb3eff3c17
rovull <yaskovetc@gmail.com>
2018-05-31 22:19:34 +0300

    Add files via upload

---
+++ user.sql
@@ -0,0 +1,15 @@
+﻿--
+-- Скрипт сгенерирован Devart dbForge Studio for MySQL, Версия 7.1.13.0
+-- Домашняя страница продукта: http://www.devart.com/ru/dbforge/mysql/studio
+-- Дата скрипта: 28.05.2018 19:18:37
+-- Версия сервера: 5.5.5-10.1.10-MariaDB
+-- Версия клиента: 4.1
+--
+
+
+SET NAMES 'utf8';
+
+INSERT INTO login_form.user(id, login, name, email, password, address, phone, education) VALUES
+(47, 'ro', 'vasya', 'yaskovetc@gmail.com', '$2y$10$o4OkT4TAJWMBP1kNrTyim.GR/.3azEuv8uKxLsHmfROu/NPkpcvOK', '57 Bullwood Rd', x'953266289', 'KHNU');
+INSERT INTO login_form.user(id, login, name, email, password, address, phone, education) VALUES
+(53, 'roma', NULL, 'yaskovetc@gmail.com', '$2y$10$DKFYOhKSZItfXgrrJBvYaekgcxMI.mQy9.hmcJenFwoRISnBMS1cC', NULL, NULL, NULL);
